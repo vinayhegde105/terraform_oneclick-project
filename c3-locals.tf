29289b080ea8adca1028a4fe9040ed7efed42ef3
vinayhegde105 <vinayhegde105@gmail.com>
2021-08-27 20:51:03 +0530

    first3

--- c3-locals.tf
+++ c3-locals.tf
@@ -18,7 +18,7 @@ locals {
 
   # Use-case-2: Common tags to be assigned to all resources
   service_name = "Demo Services"
-  owner = "Kalyan Reddy Daida"
+  owner = "Vinay hegde"
   common_tags = {
     Service = local.service_name
     Owner   = local.owner
--- c5-virtual-network.tf
+++ c5-virtual-network.tf
@@ -2,15 +2,15 @@
 resource "azurerm_virtual_network" "myvnet" {
   name                = local.vnet_name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.vth-myrg.location
+  resource_group_name = azurerm_resource_group.vth-myrg.name
   tags = local.common_tags
 }
 
 # Create Subnet
 resource "azurerm_subnet" "mysubnet" {
   name                 = local.snet_name
-  resource_group_name  = azurerm_resource_group.myrg.name
+  resource_group_name  = azurerm_resource_group.vth-myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
@@ -18,8 +18,8 @@ resource "azurerm_subnet" "mysubnet" {
 # Create Public IP Address
 resource "azurerm_public_ip" "mypublicip" {
   name                = local.pip_name
-  resource_group_name = azurerm_resource_group.myrg.name
-  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.vth-myrg.name
+  location            = azurerm_resource_group.vth-myrg.location
   allocation_method   = "Static"
   domain_name_label = "app1-${terraform.workspace}-${random_string.myrandom.id}"
   tags = local.common_tags
@@ -28,8 +28,8 @@ resource "azurerm_public_ip" "mypublicip" {
 # Create Network Interface
 resource "azurerm_network_interface" "myvmnic" {
   name                = local.nic_name
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.vth-myrg.location
+  resource_group_name = azurerm_resource_group.vth-myrg.name
 
   ip_configuration {
     name                          = "internal"
@@ -38,29 +38,4 @@ resource "azurerm_network_interface" "myvmnic" {
     public_ip_address_id = azurerm_public_ip.mypublicip.id 
   }
   tags = local.common_tags
-}
-resource "azurerm_network_security_group" "vth" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.vth-myrg.name
-  tags                = local.common_tags
-}
-
-resource "azurerm_network_security_rule" "vthnsg" {
-  name                        = "test123"
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_ranges      = ["8000", "8080"]
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.vth-myrg.name
-  network_security_group_name = azurerm_network_security_group.vth.name
-}
-
-resource "azurerm_network_interface_security_group_association" "vthanisga" {
-    network_interface_id      = azurerm_network_interface.myvmnic.id
-    network_security_group_id = azurerm_network_security_group.vth.id
 }
