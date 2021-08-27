e04ed275ea004dd5036bfd193e6a5cb59fecfe21
vinayhegde105 <vinayhegde105@gmail.com>
2021-08-27 10:20:19 +0530

    first3

--- c4-resource-group.tf
+++ c4-resource-group.tf
@@ -1,5 +1,5 @@
 # Resource-1: Azure Resource Group
-resource "azurerm_resource_group" "myrg" {
+resource "azurerm_resource_group" "vth-myrg" {
   name = local.rg_name
   location = var.resoure_group_location
   tags = local.common_tags
