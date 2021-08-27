8503ac2286926bcecfd0d291a232ceabbc1b060a
vinayhegde105 <vinayhegde105@gmail.com>
2021-08-27 10:15:45 +0530

    first

--- c2-variables.tf
+++ c2-variables.tf
@@ -15,7 +15,7 @@ variable "environment" {
 variable "resoure_group_name" {
   description = "Resource Group Name"
   type = string
-  default = "myrg"
+  default = "vthmyrg"
 }
 # 4. Resource Group Location
 variable "resoure_group_location" {
@@ -26,35 +26,27 @@ variable "resoure_group_location" {
 # 5. Virtual Network Name
 variable "virtual_network_name" {
   description = "Virtual Network Name"
-  type = string 
+  type = string
   default = "myvnet"
 }
 # 6. Subnet Name
 variable "subnet_name" {
   description = "Virtual Network Subnet Name"
-  type = string 
+  type = string
 }
 # 7. Public IP Name
 variable "publicip_name" {
   description = "Public IP Name"
-  type = string 
+  type = string
 }
 # 8. Network Interfance
 variable "network_interface_name" {
   description = "Network Interface Name"
-  type = string 
+  type = string
 }
 
 # 9. Virtual Machine Name
 variable "virtual_machine_name" {
   description = "Virtual Machine Name"
-  type = string 
+  type = string
 }
-
-
-
-
-
-
-
-
