d3701c47f1b9e378302391cd91068bf7bc6fff9e
vinayhegde105 <vinayhegde105@gmail.com>
2021-08-29 21:39:29 +0530

    test1

--- scripts/command.sh
+++ scripts/command.sh
@@ -1,17 +1,16 @@
-runcmd:
-  - sudo yum update -y
-  - sudo yum install git -y
-  - sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-  - sudo dnf list docker-ce
-  - sudo dnf install docker-ce --nobest -y
-  - sudo systemctl start docker
-  - sudo systemctl enable docker
-  - sudo docker --version
-  - sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  - sudo chmod +x /usr/local/bin/docker-compose
-  - sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-  - sudo docker-compose --version
-  - cd /home/azureuser
-  - sudo git clone https://github.com/vinayhegde105/GreatKart.git
-  - sudo docker-compose up -d
-  - sudo dnf update -y
+#!/bin/bash
+sudo yum install git -y
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf list docker-ce
+sudo dnf install docker-ce --nobest -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo docker-compose --version
+cd /home/azureuser
+sudo git clone https://github.com/vinayhegde105/GreatKart.git
+sudo docker-compose up -d
+sudo dnf update -y
